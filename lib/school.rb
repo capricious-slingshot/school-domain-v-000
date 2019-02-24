2842fd2728164fc46fb85156c19e60ac80ad18c7
Carden Z <ephemere@protonmail.com>
2019-02-24 05:31:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,10 @@ class	School
 	end
 
 	def add_student(name, grade)
-		@roster[grade] = [name]
+		if @roster.has_key?(grade)
+		  @roster[grade] << name
+		else
+			@roster[grade] = [name]
+		end
 	end
 end
